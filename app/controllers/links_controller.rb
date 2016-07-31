851e4226d92ddbec9e2b4b41ada60ca51ca89e3e
Jack Shales <jackshalesmusic@gmail.com>
2016-07-31 18:25:41 -0500

    add new and create actions and view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,18 @@ class LinksController < ApplicationController
     @links = Link.all
     render 'index.html.erb'
   end
+
+  def new
+    @link = Link.new
+    render 'new.html.erb'
+  end
+
+  def create
+    link = Link.new(
+      slug: params[:slug],
+      target_url: params[:target]
+    )
+    link.save
+    redirect_to '/links'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
  end
 
   get '/links' => 'links#index'
+  get '/links/new' => 'links#new'
+  post '/links' => 'links#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
